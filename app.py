bc3e7a6f815cdf086796f387d4e4d35b1821e390
OmarBZ27 <omarbuitron71@gmail.com>
2026-06-11 16:14:16 -0500

    Crear tablas postgres

--- app.py
+++ app.py
@@ -341,5 +341,44 @@ def eliminar_partido(id):
 
     return redirect("/partidos")    
 
+@app.route("/crear-tablas")
+def crear_tablas():
+
+    conexion = get_connection()
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS partidos (
+        id SERIAL PRIMARY KEY,
+        local VARCHAR(100) NOT NULL,
+        visitante VARCHAR(100) NOT NULL,
+        fecha VARCHAR(100) NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS pronosticos (
+        id SERIAL PRIMARY KEY,
+        partido_id INTEGER NOT NULL,
+        participante_id INTEGER NOT NULL,
+        gol_local INTEGER NOT NULL,
+        gol_visitante INTEGER NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS resultados (
+        id SERIAL PRIMARY KEY,
+        partido_id INTEGER UNIQUE,
+        gol_local INTEGER NOT NULL,
+        gol_visitante INTEGER NOT NULL
+    )
+    """)
+
+    conexion.commit()
+    conexion.close()
+
+    return "Tablas creadas"
+
 if __name__ == "__main__":
     app.run(debug=True)
